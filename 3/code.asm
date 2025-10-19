d94a820707b7d020c420fd740f9d22716cde4bd6
Alexander Timoshkin <82755751+alextim123@users.noreply.github.com>
2025-10-19 23:59:16 +0300

    Implement factorial calculation in assembly

---
+++ 3/code.asm
@@ -0,0 +1,54 @@
+.text
+.globl main
+.include "macros.s"
+
+main:
+    li t0, 1
+loop:
+    mv a0, t0
+    FAC
+    beqz a0, done
+    addi t0, t0, 1
+    j loop
+
+done:
+    addi a0, t0, -1
+    li a7, 1
+    ecall
+    li a7, 10
+    ecall
+
+factorial:
+    beqz a0, base
+    li t1, 1
+    beq a0, t1, base
+
+    addi sp, sp, -8
+    sw ra, 4(sp)
+    sw a0, 0(sp)
+
+    addi a0, a0, -1
+    jal ra, factorial
+    beqz a0, overflow
+
+    lw t2, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+
+    li t3, -1
+    divu t4, t3, t2
+    bgtu a0, t4, overflow_mul
+
+    mul a0, a0, t2
+    jr ra
+
+base:
+    li a0, 1
+    jr ra
+
+overflow:
+    lw ra, 4(sp)
+    addi sp, sp, 8
+overflow_mul:
+    li a0, 0
+    jr ra
